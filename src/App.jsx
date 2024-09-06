a92769e6837bf70977c47bcc12418860c8b9aade
Sourav <souravpramanick1997@gmail.com>
2024-09-07 02:39:59 +0530

    SWIMLANE-UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Swimlane from './components/Swimlane'
 import TakeInput from './components/TakeInput'
 import './App.css'
+import docIcon from './assets/docIcon.svg'
 function App() {
-  
+  let link = document.querySelector("link[rel='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+  }
+  link.href = docIcon;
   return (
     <>
     <TakeInput/>
