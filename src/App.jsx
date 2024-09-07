a04c82179b1f92d0e043c838e1873b6e2a7507ae
Sourav <souravpramanick1997@gmail.com>
2024-09-07 16:49:20 +0530

    Removed 2 unnecessary css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Swimlane from './components/Swimlane'
 import TakeInput from './components/TakeInput'
-import './App.css'
+
 import docIcon from './assets/docIcon.svg'
 function App() {
   let link = document.querySelector("link[rel='icon']");
